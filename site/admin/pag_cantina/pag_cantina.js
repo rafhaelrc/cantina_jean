9d0f74ea3659d5d471af7942133cb47b7d8c8756
Gabriel-Athaydes <gabrielpereiradeathaydesppp@gmail.com>
2026-01-27 12:19:10 -0300

    inicio doc básica + inicio Pupup's + organização botões + form's edição e criação

--- site/admin/pag_cantina/pag_cantina.js
+++ site/admin/pag_cantina/pag_cantina.js
@@ -1,58 +1,64 @@
 let tabela = document.getElementById('bodyTabela');
-let objetos = [
-    {
+let objetos = {
+    0: {
+        id: 0,
         nome: 'COCA 600 ML',
         categoria: 'Bebidas',
         descricao: 'Garrafa de Coca-Cola de 600 ml',
-        foto: 'URL',
-        status: 'Ativo',
+        foto: 'Lapis.svg',
+        status: 1,
         preco: 10,
     },
 
-    {
+    1: {
+        id: 1,
         nome: 'Torrada completa',
         categoria: 'Categoria',
         descricao: 'Torrada com ovo, queijo, presunto, salada e tomate',
-        foto: 'URL',
-        status: 'Ativo',
+        foto: 'Lupa.svg',
+        status: 1,
         preco: 0,
     },
 
-    {
+    2: {
+        id: 2,
         nome: 'Coxinha de frango',
         categoria: 'Categoria',
         descricao: 'Coxinha de frango frita',
-        foto: 'URL',
-        status: 'Ativo',
+        foto: 'Lapis.svg',
+        status: 1,
         preco: 0,
     },
 
-    {
+    3: {
+        id: 3,
         nome: 'Brigadeiro',
         categoria: 'Categoria',
         descricao: 'Brigadeiro caseiro',
-        foto: 'URL',
-        status: 'Ativo',
+        foto: 'fotoPadraoProduto.svg',
+        status: 1,
         preco: 0,
     },
 
-    {
+    4: {
+        id: 4,
         nome: 'Morango do amor',
         categoria: 'Categoria',
         descricao: 'Morango com Cobertura caramelizada',
-        foto: 'URL',
-        status: 'Inativo',
+        foto: 'fotoPadraoProduto.svg',
+        status: 0,
         preco: 0,
     },
-];
+};
 
 let objetos_html = ``;
 
-for (var i = 0; i < objetos.length; i++) {
+for (var i = 0; i < Object.keys(objetos).length; i++) {
     objetos_html += `<tr>
-        <td class="table-active">${objetos[i].nome}</td>
+        <input type="number" value='${i}' id="idProduto${i}" hidden>
+        <td>${objetos[i].nome}</td>
         <td>${objetos[i].descricao}</td>
-        <td class="table-active">${objetos[i].status}</td>
+        <td class="areaStatus"><span style="background-color: ${objetos[i].status ? "var(--verde-secundario)" : "#ffd600"};">${objetos[i].status ? "Ativo" : "Inativo"}</span></td>
         <td class="areaBotoes">
             <a href="#popupVisualizar"><button class="botaoVisualizar" onclick="Visualizar(${i})"><img src="Lupa.svg" alt="Visualizar" width="25px" height="25px"></button></a>
             <a href="#popupVerificar"><button class="botaoEditar" onclick="Editar(${i})"><img src="Lapis.svg" alt="Editar" width="25px" height="25px"></button></a>
@@ -64,17 +70,48 @@ for (var i = 0; i < objetos.length; i++) {
 tabela.innerHTML = objetos_html;
 
 function Cadastrar(){
-    console.log(objetos[num].nome)
+    nome = document.getElementById('nome');
+    categoria = document.getElementById('categoria');
+    descricao = document.getElementById('descricao');
+    disponibilidade = document.getElementById('disponibilidade'); // => 0 ou 1: Representa status
+    enviarFoto = document.getElementById('enviarFoto');
+    preco = document.getElementById('preco');
+    imgCadastrada = document.getElementById('imgCadastrada');
+
+    nome.value = '';
+    categoria.value = '';
+    descricao.value = '';
+    disponibilidade.value = 1;
+    preco.value = 0;
+    imgCadastrada.src = 'fotoPadraoProduto.svg';
 }
 
 function Visualizar(num){
-    console.log(objetos[num].nome)
+    disponibilidade = document.getElementById('disponibilidade');
+    console.log(disponibilidade.value);
 }
 
 function Editar(num){
-    console.log(objetos[num].nome)
+    nome = document.getElementById('nome');
+    categoria = document.getElementById('categoria');
+    descricao = document.getElementById('descricao');
+    disponibilidade = document.getElementById('disponibilidade'); // => 0 ou 1: Representa status
+    enviarFoto = document.getElementById('enviarFoto');
+    preco = document.getElementById('preco');
+    imgCadastrada = document.getElementById('imgCadastrada');
+
+    nome.value = objetos[num].nome;
+    categoria.value = objetos[num].categoria;
+    descricao.value = objetos[num].descricao;
+    disponibilidade.value = objetos[num].status;
+    preco.value = objetos[num].preco;
+    imgCadastrada.src = objetos[num].foto;
 }
 
 function Excluir(num){
-    console.log(objetos[num].nome)
+    console.log(objetos[num].nome);
+}
+
+function Salvar() {
+    
 }
